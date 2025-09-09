2de6ed0f1b307a97be3ebece0a0350b06a3a0d8a
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2025-09-09 10:09:18 +0500

    Update next.config.ts to include remote image patterns for suslop.wasmer.app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   images: {
+//     domains: ["suslop.wasmer.app"], // ✅ allow WP media domain
+//   },
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'suslop.wasmer.app',
+        port: '',
+        pathname: '/wp-content/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'suslop.wasmer.app',
+        port: '',
+        pathname: '/wp-content/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
